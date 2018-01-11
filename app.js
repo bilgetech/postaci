60967dcc14bb6277ac76f8f5f28a87a9d58cc71e
Ahmet Safa Orhan <safa.orhan@bilgetech.com.tr>
2018-01-11 11:59:41 +0300

    Add dependencies and create app structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+// Load the variables in .env file to the process.env
+dotenv.config();
+
+// Configure Middlewares
+app.use(morgan(process.env.LOGGING_LEVEL || 'tiny'));
+app.use(bodyParser.json());
+
+// Configure Routes
+app.use('/', require('./routes/index'));
