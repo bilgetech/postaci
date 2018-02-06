9269dbeca00bb978a83f9e59b5c1c0259c1919e7
Ahmet Safa Orhan <safa.orhan@bilgetech.com.tr>
2018-02-06 15:28:18 +0300

    Add scheduled runs

--- components/boxes.js
+++ components/boxes.js
@@ -1,17 +1,30 @@
 const Box = require('./box');
+const scheduler = require('node-schedule');
 
 const items = [];
+
+function findBoxByAddress(address) {
+  return items.find(box => box.address === address);
+}
 
 function init(boxesArr, cb) {
   boxesArr.forEach((opts) => {
     const box = new Box(opts);
     items.push(box);
     box.refreshAndRun();
-  });
-}
 
-function findBoxByAddress(address) {
-  return items.find(box => box.address === address);
+    if (opts.scheduleConfig) {
+      scheduler.scheduleJob(opts.scheduleConfig, (fireDate) => {
+        console.log(
+          'Triggering scheduled run for box:',
+          opts.address,
+          'at:',
+          fireDate.toTimeString(),
+        );
+        findBoxByAddress(opts.address).run();
+      });
+    }
+  });
 }
 
 const Boxes = {};
